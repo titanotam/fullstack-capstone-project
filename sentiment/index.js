bd4f379c9e90c2a0be0028074833ced895adf08d
huynhnhuttam <huynhnhuttam@gmail.com>
2026-06-08 12:27:31 -0400

    update

--- sentiment/index.js
+++ sentiment/index.js
@@ -40,7 +40,7 @@ app.post('/sentiment', async (req, res) => {
         // Task 5: set sentiment to negative or positive based on score rules
         if (analysisResult < 0) {
             sentiment = "negative";
-        } else if (analysisResult > 0) {
+        } else if (analysisResult > 0.33) {
             sentiment = "positive";
         }
 
